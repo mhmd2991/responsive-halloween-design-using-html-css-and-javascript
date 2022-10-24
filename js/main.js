3e74e25b2d5990be8366904b6f0b8c6599117287
Mohamad Salimi <mohamad.salimi@hotmail.com>
2022-10-24 19:03:31 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,40 @@
 let btnMenu = document.querySelector('.toggle-btn'),
-    menu = document.querySelector('.menu');
+    menu = document.querySelector('.menu'),
+    sections=document.querySelectorAll('section'),
+    navLi = document.querySelectorAll('.navbar-left li a'),
+    toTopBtn = document.querySelector('#to-top');
 
 btnMenu.addEventListener('click', () => {
     menu.classList.toggle('active');
+});
+
+window.onscroll = function () {
+    if (this.scrollY >= 300) {
+        toTopBtn.classList.add('show');
+    } else {
+        toTopBtn.classList.remove('show');
+    }
+}
+
+toTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+});
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if (pageYOffset >= sectionTop - 100) {
+            current = section.getAttribute('id');
+        }
+    });
+    navLi.forEach(li => {
+        li.classList.remove("active");
+        if (li.dataset.link == current) {
+            li.classList.add('active');
+        }
+    });
 });
